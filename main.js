8f4d93c18aa265e4f3bbe9f4d67a1cadb729439d
flbusom <latorre.ferran@gmail.com>
2021-01-21 15:24:51 +0100

    main

--- main.js
+++ main.js
@@ -35,3 +35,32 @@ document.addEventListener("DOMContentLoaded", () => {
         game.start();
     }
 );
+
+score = 0;
+
+function go(x){
+  $({score: 0}).animate({score: x},{
+    duration: 1000,
+    easing:"linear",
+    step: function(now, fx){
+      $("#score").html(score + Math.floor(now));
+    },
+    queue:false,
+    complete: function(now, fx){
+      score += x;
+    }
+  });
+  $("#tag").fadeIn({
+    duration:700,
+    easing:"linear",
+    step:function(now, fx){
+      $(this).css("top", -55 * now  +"px");
+    }
+  }).fadeOut({
+    duration:300,
+    step:function(now, fx){
+      $(this).css("top",-55 * ( 2 - now) + "px");
+    }
+  });
+
+}
